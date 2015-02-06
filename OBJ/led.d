956e8cdc7122305f2b51b45a7e03ebaa9c0c047f
ibanezjem <ibanezjem@qq.com>
2015-02-06 22:04:49 +0800

    Delete Obj

--- OBJ/led.d
+++
@@ -1,8 +0,0 @@
-..\OBJ\led.o: ..\HARDWARE\LED\led.c
-..\OBJ\led.o: ..\HARDWARE\LED\led.h
-..\OBJ\led.o: ..\SYSTEM\sys\sys.h
-..\OBJ\led.o: C:\Keil\ARM\INC\ST\STM32F10x\stm32f10x_map.h
-..\OBJ\led.o: C:\Keil\ARM\INC\ST\STM32F10x\stm32f10x_conf.h
-..\OBJ\led.o: C:\Keil\ARM\INC\ST\STM32F10x\stm32f10x_type.h
-..\OBJ\led.o: C:\Keil\ARM\INC\ST\STM32F10x\cortexm3_macro.h
-..\OBJ\led.o: C:\Keil\ARM\INC\ST\STM32F10x\stm32f10x_nvic.h
--- OBJ/sys.d
+++
@@ -1,7 +0,0 @@
-..\OBJ\sys.o: ..\SYSTEM\sys\sys.c
-..\OBJ\sys.o: ..\SYSTEM\sys\sys.h
-..\OBJ\sys.o: C:\Keil\ARM\INC\ST\STM32F10x\stm32f10x_map.h
-..\OBJ\sys.o: C:\Keil\ARM\INC\ST\STM32F10x\stm32f10x_conf.h
-..\OBJ\sys.o: C:\Keil\ARM\INC\ST\STM32F10x\stm32f10x_type.h
-..\OBJ\sys.o: C:\Keil\ARM\INC\ST\STM32F10x\cortexm3_macro.h
-..\OBJ\sys.o: C:\Keil\ARM\INC\ST\STM32F10x\stm32f10x_nvic.h
